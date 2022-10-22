4c20ebf5c8618ba6186986790f5b129eaeb4526f
Grupo-Hotel <115844790+Grupo-Hotel@users.noreply.github.com>
2022-10-21 21:57:08 -0300

    Update javascriptcontacto.js

--- css/javascriptcontacto.js
+++ css/javascriptcontacto.js
@@ -1,5 +1,6 @@
- // La siguiente funcion valida el elemento input
- function validarUserName() {
+// JavaScript source code
+// La siguiente funcion valida el elemento input
+function validarUserName() {
 
     // El input que queremos validar
     const input = document.getElementById('userName');
@@ -12,9 +13,9 @@
     const pattern = new RegExp('^[A-Z ]+$', 'i');
 
     return validar(input, message,pattern, maximo);
-  }
+}
 
-  function validarUserLastName() {
+function validarUserLastName() {
 
     // El input que queremos validar
     const input = document.getElementById('userLastName');
@@ -27,9 +28,9 @@
     const pattern = new RegExp('^[A-Z]+$', 'i');
 
     return validar(input, message,pattern, maximo);
-  }
+}
 
-  function validaruserEmail() {
+function validaruserEmail() {
 
     // El input que queremos validar
     const input = document.getElementById('userEmail');
@@ -42,9 +43,9 @@
     const pattern = new RegExp("^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$"); 
 
     return validar(input, message,pattern, maximo);
-  }
+}
 
-  function validarUserCountry() {
+function validarUserCountry() {
 
     // El input que queremos validar
     const input = document.getElementById('userCountry');
@@ -57,9 +58,9 @@
     const pattern = new RegExp('^[A-Z]+$', 'i');
 
     return validar(input, message,pattern, maximo);
-  }
+}
 
-  function validarUserCity() {
+function validarUserCity() {
 
     // El input que queremos validar
     const input = document.getElementById('userCity');
@@ -72,9 +73,9 @@
     const pattern = new RegExp('^[A-Z]+$', 'i');
 
     return validar(input, message,pattern, maximo);
-  }
+}
 
-  function validarUserAddress() {
+function validarUserAddress() {
 
     // El input que queremos validar
     const input = document.getElementById('userAddress');
@@ -87,58 +88,54 @@
     const pattern = new RegExp('^[A-Z]+$', 'i');
 
     return validar(input, message,pattern, maximo);
-  }
+}
 
 
 
   
-  function validar(input, message, pattern, maximo) {
+function validar(input, message, pattern, maximo) {
     // Variable que usaremos para determinar si el input es valido
     let isValid = false;
 
     // Primera validacion, si input esta vacio entonces no es valido
     if(!input.value) {
-      isValid = false;
-    } else {
-      // Segunda validacion, si input es mayor que 35
-      if(input.value.length > maximo) {
         isValid = false;
-      } else {
-        // Tercera validacion, si input contiene caracteres diferentes a los permitidos
-        if(!pattern.test(input.value)){ 
-        // Si queremos agregar letras acentuadas y/o letra ñ debemos usar
-        // codigos de Unicode (ejemplo: Ñ: \u00D1  ñ: \u00F1)
-          isValid = false;
-        } else {
-          // Si pasamos todas la validaciones anteriores, entonces el input es valido
-          isValid = true;
-        }
-      }
-    }
-
-    //Ahora coloreamos el borde de nuestro input
-    if(!isValid) {
-      // rojo: no es valido
-      input.style.borderColor = 'salmon'; // me parece que 'salmon' es un poco menos agresivo que 'red'
-      // mostramos mensaje
-      message.hidden = false;
     } else {
-      // verde: si es valido
-      input.style.borderColor = 'palegreen'; // 'palegreen' se ve mejor que 'green' en mi opinion
-      // ocultamos mensaje;
-      message.hidden = true;
+        // Segunda validacion, si input es mayor que 35
+        if(input.value.length > maximo) {
+            isValid = false;
+        } else {
+            // Tercera validacion, si input contiene caracteres diferentes a los permitidos
+            if(!pattern.test(input.value)){ 
+            // Si queremos agregar letras acentuadas y/o letra ñ debemos usar
+            // codigos de Unicode (ejemplo: Ñ: \u00D1  ñ: \u00F1)
+            isValid = false;
+            } else {
+            // Si pasamos todas la validaciones anteriores, entonces el input es valido
+            isValid = true;
+            }
+        }
     }
+}
 
-    // devolvemos el valor de isValid
-    return isValid;
+//Ahora coloreamos el borde de nuestro input
+if(!isValid) {
+    // rojo: no es valido
+    input.style.borderColor = 'salmon'; // me parece que 'salmon' es un poco menos agresivo que 'red'
+    // mostramos mensaje
+    message.hidden = false;
+} else {
+    // verde: si es valido
+    input.style.borderColor = 'palegreen'; // 'palegreen' se ve mejor que 'green' en mi opinion
+    // ocultamos mensaje;
+    message.hidden = true;
+}
+
+// devolvemos el valor de isValid
+return isValid;
    
-   document.addEventListener('DOMContentLoaded', function(){
-    let formulario = document.getElementById('formul');
-    formulario.addEventListener('submit', function() {
-      formulario.reset();
-    });
-  });
-  }
-
-  
-
+   
+function getName() {
+    var formulario = document.getElementById('formul');
+    formulario.reset(); 
+}
